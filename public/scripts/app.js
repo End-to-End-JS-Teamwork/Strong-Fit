bd666b3ba21795b9ccea040fcc4d4e9f07d2ce77
tddold <tdd@dekom.bg>
2016-01-08 02:30:50 +0200

    Add register.jade and login.jade

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,30 +1,32 @@
-'use strict';
+(function () {
+    'use strict';
 
-var strongFitApp = angular
-    .module('strongFitApp', ['ngResource', 'ngRoute', 'ngCookies'])
-    .config(function($routeProvider) {
-        var routeUserCheck = {
-            adminRole: {
-                authenticate: function(auth) {
-                    return auth.isAuthorizedForRole('admin');
+    var strongFitApp = angular
+        .module('strongFitApp', ['ngResource', 'ngRoute', 'ngCookies'])
+        .config(function($routeProvider) {
+            var routeUserCheck = {
+                adminRole: {
+                    authenticate: function(auth) {
+                        return auth.isAuthorizedForRole('admin');
+                    }
+                },
+                authenticated: {
+                    authenticate: function(auth) {
+                        return auth.isAuthenticated();
+                    }
                 }
-            },
-            authenticated: {
-                authenticate: function(auth) {
-                    return auth.isAuthenticated();
-                }
+            };
+        })
+        .value('toastr', toastr)
+        .constant('appName', 'StrongFit')
+        .constant('serverPath', 'http://localhost:3210');
+
+    strongFitApp.run(function($rootScope, $location) {
+        // When failed to change route -> change location path
+        $rootScope.$on('$routeChangeError', function(ev, current, previous, rejection) {
+            if (rejection === 'not authorized') {
+                $location.path('/unauthorized');
             }
-        };
-    })
-    .value('toastr', toastr)
-    .constant('appName', 'StrongFit')
-    .constant('serverPath', 'http://localhost:3210');
-
-strongFitApp.run(function($rootScope, $location) {
-    // When failed to change route -> change location path
-    $rootScope.$on('$routeChangeError', function(ev, current, previous, rejection) {
-        if (rejection === 'not authorized') {
-            $location.path('/unauthorized');
-        }
+        });
     });
-});
+}());
