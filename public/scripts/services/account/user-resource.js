76b179b1402c28b62daaf43ddfe5e38ec5d08ed9
dimitarsd <dsd363978@yahoo.com>
2016-01-09 19:34:37 +0200

    Removed scripts folder - we don't need it

--- public/scripts/services/account/user-resource.js
+++
@@ -1,9 +0,0 @@
-strongFitApp.factory('UsersResource', function($resource) {
-    var UsersResource = $resource('/api/users/:id', {_id:'@id'}, { update: {method: 'PUT', isArray: false}});
-
-    UsersResource.prototype.isAdmin = function() {
-        return this.roles && this.roles.indexOf('admin') > -1;
-    };
-
-    return UsersResource;
-});
