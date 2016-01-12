6bba4e0006743ad13e3059e51e189ffcfae63569
radostin-angelov <angelovradostin@gmail.com>
2016-01-12 04:14:25 +0200

    Update subcategoryController.js

--- server/controllers/subcategoryController.js
+++ server/controllers/subcategoryController.js
@@ -15,38 +15,4 @@ module.exports = {
                 res.render('partials/forum/subcategory', {subcategory: subcategory});
             });
     }
-    /*getForumMainData: function (req, res) {
-        Category
-            .find({})
-            .exec(function (err, categories) {
-                if (err) {
-                    console.log('Error while getting categories forum data: ' + err);
-                    return;
-                }
-
-                Subcategory
-                    .find({})
-                    .exec(function (err, subcategories) {
-                        if (err) {
-                            console.log('Error while getting subcategories forum data: ' + err);
-                            return;
-                        }
-
-                        Comment
-                            .find({})
-                            .exec(function (err, comments) {
-                                if (err) {
-                                    console.log('Error while getting comments forum data: ' + err);
-                                    return;
-                                }
-
-                                var viewModelResult = viewModels
-                                    .ForumHomeViewModel
-                                    .getForumHomeViewModel(categories, subcategories, comments);
-
-                                res.render('partials/forum/forum-home', {result: viewModelResult});
-                            });
-                    });
-            });
-    }*/
 };
